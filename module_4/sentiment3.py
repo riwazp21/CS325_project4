336245bc7d45088013671803c1b86c2ce4accdf2
riwazp21 <82861337+riwazp21@users.noreply.github.com>
2023-11-09 09:30:19 -0600

    file description added

--- module_4/sentiment3.py
+++ module_4/sentiment3.py
@@ -1,3 +1,14 @@
+"""
+File Name: sentiment3.py
+Author: Riwaz Poudel & Justin Burns
+Input: Comments or any text that needs a sentiment analysis
+Output: sentiment.txt in Data/sentiment
+Functionality
+1. The function get_sentiment is imported in run.py
+2. It calls the textblob API to conduct a sentiment analysis
+3. The sentiment of the sentence is returned as either Positive, Negative, or Neutral
+
+"""
 from textblob import TextBlob
 
 # Create a list of sentences to analyze
