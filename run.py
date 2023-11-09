36ba087e9caf6d76d376393cce2b412ab7d2a4f8
riwazp21 <82861337+riwazp21@users.noreply.github.com>
2023-11-09 09:26:38 -0600

    Updated the file description

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 """
 File Name: run.py
 Author: Riwaz Poudel
-Input: reddit URL through command line argument
-Output: content.txt and comment.txt inside Data/Raw and inside Data/Processed respectively
+Input: text file that has a reddit url through command line argument
+Output: content1.txt, comment1.txt, and sentiment.txt inside Data/Raw, inside Data/Processed, and Data/sentiment respectively
 Functionality:
 1. This is the main file of the entire program. 
 2. This file receives the reddit link through the command line argument
 3. The file imports the function content_scraper and comment_scraper from module 1 and module 2
 4. The reddit link is passed through the content_scraper
 5. The raw file is passed through the comment_scraper
-6. The output from this file would be both the raw and processed file of the reddit link that includes html content, and the comments
+6. The output from this file would be both the raw and processed file of the reddit link that includes html content, and the comments. It will also output the sentiment analysis of the first 50 comments
 
 Basic structure of how the modules work and interact with each other
 CS325_p3/
@@ -20,8 +20,11 @@ CS325_p3/
 |	└──comment_scraper.py
 |--module_3
 	└──file_storer.py
+|--module_4
+        └──sentiment.py
 run.py calls functions content_scraper and comment_scraper from module 1 and module 2
 content_scraper.py(module_1) and comment_scraper.py(module_2) calls file_storer.py from module_3
+After the contents and comments are stored succesfully, run.py calls sentiment.py(module_4) to get the sentiments of a passed comment
 
 """
 from bs4 import BeautifulSoup
